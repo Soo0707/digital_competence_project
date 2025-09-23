bbb90dced0287c612ae79f606914ef0b26fff419
SOO <ssj060707+github@gmail.com>
2025-09-23 18:11:58 +0800

    updated build script and included source files for cross compilation with windows

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-g++ main.cpp -o /mnt/tmpfs/a.out -lsqlite3
+# TARGET LINUX
+# only works if sqlite is already built
+# if headers were provided by the distro package manager, change #include "sqlite3.h" to #include <sqlite3.h>
+# g++ main.cpp -o /mnt/tmpfs/a.out -lsqlite3
+
+# TARGET WINDOWS
+
+x86_64-w64-mingw32-gcc -c sqlite3.c -o lib/sqlite3.o # build sqlite with C, obtained from the amalgamation package on sqlite's site
+x86_64-w64-mingw32-ar rcs lib/libsqlite3.a lib/sqlite3.o # turn blob to archive
+x86_64-w64-mingw32-g++ main.cpp -o /mnt/tmpfs/out.exe -Iinclude/ -Llib/ -lsqlite3 -static # build project with sqlite
